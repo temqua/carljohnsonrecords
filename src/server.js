80f6c63bf3ccf29b5baf291f92d5f6e7ef3f5360
artem <art_nazarov@mail.ru>
2023-02-20 01:05:55 +0300

    Created dist dir. Removed nunjucks.

--- src/server.js
+++ src/server.js
@@ -1,19 +1,14 @@
 const path = require("path");
 const express = require("express");
-const nunjucks = require('nunjucks');
 const db = require("./db");
 const app = express();
 const port = process.env.PORT || 5090;
-app.use(express.static(path.join(__dirname, "..", "public")));
-nunjucks.configure('public', {
-    autoescape: true,
-    express: app
-});
+app.use(express.static(path.join(__dirname, "..", "dist")));
 app.get("/", async function (request, response, next) {
   // await db.connect(function (result) {
   //   console.log("result: ", result)
   // });
-  response.render("index.html");
+  response.send("index.html");
   next();
 });
 app.listen(port);
