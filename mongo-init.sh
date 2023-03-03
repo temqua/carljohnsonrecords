c6abf126e77763e11d04ada0b28def3280e37991
Artem Nazarov <art_nazarov@mail.ru>
2023-03-03 19:45:28 +0300

    Updated init script

--- mongo-init.sh
+++ mongo-init.sh
@@ -22,4 +22,34 @@ db.createUser({
   }]
 })
 db.createCollection('general')
+db.general.insert({
+  pricesHeader: 'Услуги и цены'
+})
+db.createCollection('prices')
+db.prices.insert( [
+		{
+			price: 350,
+			title: 'Репетиция группы',
+			src: 'rehearsal.png',
+			alt: 'Band rehearsal'
+		},
+		{
+			price: 500,
+			title: 'Запись вокала/инструмента',
+			src: 'instrumentrec.png',
+			alt: 'Instrument or vocals recording services'
+		},
+		{
+			price: 750,
+			title: 'Запись группы лайвсетом',
+			src: 'livesetrec.png',
+			alt: 'Live set recording services'
+		},
+		{
+			price: 2000,
+			title: 'Сведение/мастеринг',
+			src: 'mixing.png',
+			alt: 'Mixing services'
+		}
+])
 EOF
