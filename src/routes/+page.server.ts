e51938097195f5c9b2325ab2208971839b55ff8d
artem <art_nazarov@mail.ru>
2023-03-04 03:07:14 +0300

    Added console.log on server

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,5 +3,6 @@ import type { PageServerLoad } from './$types';
 export const load = (async () => {
 	const prices = await Db.getCollection('prices');
 	const general = await Db.getCollection('general');
+	console.log('prices :>> ', prices);
 	return { prices, general };
 }) satisfies PageServerLoad;
